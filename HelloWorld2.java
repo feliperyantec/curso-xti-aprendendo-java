5dddae8f4b458f270d96c6d5933357187148ccdc
feliperyanribeirooliveira <felipe.ryan.tec@gmail.com>
2019-10-28 22:59:25 -0300

    HelloWorld2 - Comentarios adicionados

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -1,3 +1,20 @@
+/**
+ * Class sempre começar com letra maiuscula - Nomeada como: "CamelCase"
+ * nome de classe com numeração no inicio gera erro de <identifier> Obs: arquivo fonte nem chega a ser compilado 
+ * "main" escrito de form incorreta gera erro no EXECUÇÃO, mas compila normalmente.
+ * classe ou método sem chave de fechamento gera erro de END OF FILE
+ * metodo sempre começar com letras minusculas e as palavras seguidas com letra maiuscula
+ */
+
+	public static void main(String[] args) {
+	
+	    String nome = "FelipeRyan";
+	    int idade = 23;
+	    boolean Solteiro = true;
+	
+	    System.out.println("Texto");
+	}
+
 public class HelloWorld2 {
 
     public static void main(String[] args) {
