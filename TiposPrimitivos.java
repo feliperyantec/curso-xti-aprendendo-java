c7e5776ad0d4d39d17f06041de9706cbfed333c7
feliperyanribeirooliveira <felipe.ryan.tec@gmail.com>
2019-10-28 23:02:43 -0300

    Tipos Primitivos

---
+++ TiposPrimitivos.java
@@ -0,0 +1,13 @@
+/**
+ * Tipos Primitivos
+ * INT - BYTE - SHORT - LONG - FLOAT - DOUBLE - CHAR - VOID - BOOLEAN
+ * IEEE 754 - Unicode
+ */
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        
+        char Sexo = 'M';
+        System.out.println(Sexo);
+    }
+}
