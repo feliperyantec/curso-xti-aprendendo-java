570bcde0cf8c5903c17851b2319a4d13bd321238
feliperyanribeirooliveira <felipe.ryan.tec@gmail.com>
2019-11-01 18:00:46 -0300

    Operadores de atribuição

---
+++ OperadoresDeAtribuicao.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+public class OperadoresDeAtribuicao {
+
+    public static void main(String[] args) {
+        
+        int x = 6;
+        x += 3;
+        x -= 3;
+        x *= 3;
+        x /= 3;
+        x %= 3;
+        System.out.println(x);
+    }
+}
