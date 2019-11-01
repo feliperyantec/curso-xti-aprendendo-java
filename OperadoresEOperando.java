2be722f641aefd07bdd469e821d357eafdb525ce
feliperyanribeirooliveira <felipe.ryan.tec@gmail.com>
2019-11-01 18:02:08 -0300

    Operadores e Operando

---
+++ OperadoresEOperando.java
@@ -0,0 +1,18 @@
+/**
+*      1, 2, 3, 4, 5... Esse são Operando;
+*       + - * / Esses são Operadores;
+*      2 + 3 = Operação-Expressão - Essa Expressão também é chamada de operador binário, pois utiliza     *      dois operando.
+*       -2 OPerador Unário - pois apera em apenas um operando.
+*      true ? "Sim" : "Não"; Operador Ternário - pois opera com 3 operando
+*/
+
+public class OperadoresEOperando {
+
+    public static void main(String[] args) {
+
+        String x = "9" + "4";
+
+        double y = 7 - 6 + 8 * 4;
+        System.out.println(y);
+    }
+}
