8d7a03736124e8a00363ac08e255e6d86392f6ac
feliperyanribeirooliveira <felipe.ryan.tec@gmail.com>
2019-11-01 18:03:30 -0300

    Operadores Matematicos

---
+++ OperadoresMatematicos.java
@@ -0,0 +1,19 @@
+/**
+ * o operador "+" pode ser usado para concatenação de Strings
+ * "%" Módulo - retorna o resto da divisão entre dois numeros
+ */
+public class OperadoresMatematicos{
+
+    public static void main(String[] args) {
+        
+        //String y = "Oi" + "Programador"; Concatenação
+        //double y = 8 % 3; Modulo
+
+        int x = 6;
+        int y = ++x; //pré-incremento
+        //int y = x++; //pós-incremento
+        //int y = --x; //pré-depremento
+        //int y = x--; //pós-depremento
+        System.out.println();
+    }
+}
