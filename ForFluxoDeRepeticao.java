b8297c1f4a687e2390ba503920a3425c790aaf09
feliperyanribeirooliveira <felipe.ryan.tec@gmail.com>
2019-11-08 09:11:22 -0400

    For - Fluxos de Repetição

---
+++ ForFluxoDeRepeticao.java
@@ -0,0 +1,13 @@
+public class ForFluxoDeRepeticao {
+
+    public static void main(String[] args) {
+        
+        int tamanho = 10;
+        for (int x=0; x<tamanho; x++) {
+            for (int i=0; i<tamanho; i++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }   
+    }
+}
