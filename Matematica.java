22a588196a9318517838803b23867945f3144b5e
feliperyanribeirooliveira <felipe.ryan.tec@gmail.com>
2019-11-13 20:51:45 -0400

    Desafio Raiz Quadra com Equação Pell

---
+++ Matematica.java
@@ -0,0 +1,12 @@
+public class Matematica {
+
+    int raiz(int numero) {
+        int raiz = 0, impar = 1;
+        while(numero >= impar) {
+            numero -= impar;
+            impar += 2;
+            ++raiz;
+        }
+        return raiz;
+    }
+}
